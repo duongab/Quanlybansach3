aba1cbc7a2a93d8465975dfe141235a879c15bf2
duongab <78307370+duongab@users.noreply.github.com>
2021-06-05 21:11:08 +0700

    LTQL456

---
+++ Models/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quanlybansach.Models
+{
+    public class Class2
+    {
+        public string machude { get; set; }
+
+        
+        public string tenchude { get; set; }
+
+        public virtual ICollection<Sach> Sachs { get; set; }
+        public HashSet<Chude> Chudes { get; }
+    }
+}
