dc87d32af45629703d0ca9e54031764f2faa1fd0
duongab <78307370+duongab@users.noreply.github.com>
2021-06-05 21:01:41 +0700

    LTQL234

---
+++ Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quanlybansach.Models
+{
+    public class Class1
+    {
+   
+        public string manxb { get; set; }
+        
+        public string tennxb { get; set; }
+
+        public string diachi { get; set; }
+
+        public string dienthoai { get; set; }
+    }
+}
